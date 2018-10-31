fa2bc9ff31d67aafba07b8789d79a8d9ee53981b
Jereme Corrado <jereme@zoion.net>
2018-10-31 16:46:08 -0400

    Filename fixes

--- spec/core_gelf_20181025_spec.rb
+++ spec/core_gelf_20181025_spec.rb
@@ -2,12 +2,11 @@ require 'json_spec'
 
 describe "core GELF 201810225" do
   before do
-    dir = "spec/fixtures/core_gelf_20181025"
-    @good_json = File.read("#{dir}/good.json")
-    @test_output_json = File.read("#{dir}/test_output.json")
+    @good_json = File.read("spec/fixtures/core_gelf_20181025/good.json")
+    @test_output_json = File.read("artifacts/test_output.json")
   end
 
-  it "produced equivalent JSON" do
+  it "yielded equivalent JSON output" do
     @good_json.json.should be_json_eql(@test_output_json)
   end
 end
